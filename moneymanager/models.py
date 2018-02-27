391317f1b8314ffe7f9ce2c7f3b016b6a63bf3ae
sanoto <kmn2wei@yahoo.co.jp>
2018-02-27 13:29:07 +0900

    [fix]

--- moneymanager/models.py
+++ moneymanager/models.py
@@ -7,4 +7,4 @@ class User(models.Model):
     name = models.CharField("名前", max_length=20)
     user_id = models.IntegerField("学籍番号", primary_key=True)
     felica_id = models.CharField("FelicaID", max_length=100)
-    money = models.IntegerField("所持金")
+    money = models.IntegerField("所持金", default=0)
