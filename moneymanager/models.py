2f0641e3f26b102cfcff80e845f1463b7d1d86a7
sanoto <kmn2wei@yahoo.co.jp>
2018-02-28 13:02:12 +0900

    [fix] 学籍番号を変更不可に

--- moneymanager/models.py
+++ moneymanager/models.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField("名前", max_length=20)
-    user_id = models.IntegerField("学籍番号", primary_key=True)
+    user_id = models.IntegerField("学籍番号", primary_key=True, read_only=True)
     felica_id = models.CharField("FelicaID", max_length=100)
     money = models.IntegerField("所持金", default=0)
